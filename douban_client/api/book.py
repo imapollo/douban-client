5d9c529a63cad5aa7367d8f48669d66f3b096296
Minjie Zhang <ze.apollo@gmail.com>
2013-11-05 08:46:23 +0000

    Add method to get all book list.

--- douban_client/api/book.py
+++ douban_client/api/book.py
@@ -11,3 +11,17 @@ class Book(Subject):
 
     def isbn(self, isbn_id):
         return self._get('/v2/book/isbn/%s'%isbn_id)
+
+    def get(self, id):
+        return self._get('/v2/book/%s'%id)
+
+    def list(self, user_id):
+        return self._get('/v2/book/user/%s/collections'%user_id)['collections']
+
+    def list_all(self, user_id):
+        list = self._get('/v2/book/user/%s/collections?count=100'%user_id)
+        total = list['total']
+        list = list['collections']
+        for start in range (100, total, 100):
+            list = list + self._get('/v2/book/user/%s/collections?count=100&start=%s' % (user_id,start) )['collections']
+        return list
